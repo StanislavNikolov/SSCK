f9014c3bd9926549cf0cbf10b73f3646f7417c86
Stanislav Nikolov <stanislav.ltb@gmail.com>
2015-08-31 08:08:14 +0300

    Fix indentation on main.js.

--- main.js
+++ main.js
@@ -7,48 +7,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var fs = require("fs");
 
+var results = {};
 app.get("/", function(req, res) {
-	  res.sendFile(__dirname+"/index.html");
+	res.sendFile(__dirname+"/index.html");
 });
 
 app.get("/results", function(req, res) {
-
+	//TODO
 });
 
 var server = app.listen(3000, function() {
-	  var host = server.address().address;
-	  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 
-	  console.log("Starting server on http://%s:%s", host, port);
+	console.log("Starting server on http://%s:%s", host, port);
 });
 
 function randStr()
 {
-	  return Math.random().toString(36).substr(2, 5);
+	return Math.random().toString(36).substr(2, 5);
 }
 
 app.post("/", function(req, res) {
-	  var dir = __dirname+"/uploadDir/"+req.body.guysName;
+	var dir = __dirname+"/uploadDir/"+req.body.guysName;
 
-	  if(fs.existsSync(dir) == false)
-		    fs.mkdirSync(dir);
+	if(fs.existsSync(dir) == false)
+		fs.mkdirSync(dir);
 
-	  dir += ("/"+req.body.task);
-	  if(fs.existsSync(dir) == false)
-		    fs.mkdirSync(dir);
+	dir += ("/"+req.body.task);
+	if(fs.existsSync(dir) == false)
+		fs.mkdirSync(dir);
 
-	  var completeFileName = dir+"/"+randStr()+".cpp";
-	  fs.writeFile(completeFileName, req.body.sourceInput, function(err) {
-		    if(err)
-		    {
-				console.log(err);
-				return;
-		    }
-		    console.log(completeFileName);
-	  });
-	  res.sendFile(__dirname+"/submissionReceived.html");
+	var completeFileName = dir+"/"+randStr()+".cpp";
+	fs.writeFile(completeFileName, req.body.sourceInput, function(err) {});
+	//res.sendFile(__dirname+"/submissionReceived.html");
 
-	  cp(__dirname+"/compile.sh "+completeFileName+" "+req.body.task, function(err, stdout, stderr) {
-		    console.log(stdout);
-	  });
+	cp(__dirname+"/compile.sh "+completeFileName+" "+req.body.task, function(err, stdout, stderr) {res.send(stdout); });
 });
