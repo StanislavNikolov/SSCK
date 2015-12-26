01b8ef40905908141eca8a6f79103497861138d8
Stanislav Nikolov <stanislav.ltb@gmail.com>
2015-12-27 01:07:16 +0200

    Cleanup compile.sh

--- compile.sh
+++ compile.sh
@@ -4,9 +4,14 @@ fullPath=$1
 task=$2
 notFullPath=${fullPath%.*}
 
-CXXFLAGS="-O2 -std=c++11"
-echo "g++ $CXXFLAGS $fullPath -o $notFullPath"
+CXXFLAGS="-O2 -std=c++14"
 
-(gcc -E $CXXFLAGS $fullPath | grep -qi fstream) && (echo "The source contains unsafe code! (SSCK_ERR_100)" && exit)
-g++ $CXXFLAGS $fullPath -o $notFullPath
+if c++ -E $CXXFLAGS $fullPath | grep -qi fstream
+then
+	echo "The source contains unsafe code! (SSCK_ERR_100)"
+	#exit
+fi
+
+echo "c++ $CXXFLAGS $fullPath -o $notFullPath"
+c++ $CXXFLAGS $fullPath -o $notFullPath
 ./checkers/$task $notFullPath
