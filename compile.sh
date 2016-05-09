95086124ddacd80a5eb3823acd2c886315c716da
Stanislav Ch. Nikolov <stanislav@cukii.tk>
2016-05-09 23:53:40 +0300

    Remove broken warning message

--- compile.sh
+++ compile.sh
@@ -7,12 +7,6 @@ notFullPath=${fullPath%.*}
 
 CXXFLAGS="-O2 -std=c++14"
 
-if c++ -E $CXXFLAGS $fullPath | grep -qi fstream
-then
-	echo "The source contains unsafe code! (SSCK_ERR_100)"
-	# exit
-fi
-
 echo "Compiling with C compiler flags: $CXXFLAGS"
 if timeout 20 c++ $CXXFLAGS $fullPath -o $notFullPath
 	then echo "Compilation succesful"
