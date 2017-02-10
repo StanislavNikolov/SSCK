ea56d7a8c9afe7f7ca06d7b47bd4675576ebf2ca
Stanislav Ch. Nikolov <stanislav@cukii.tk>
2017-02-10 18:06:10 +0200

    Report non zero exit codes as CRSH insted of time limit

--- compile.sh
+++ compile.sh
@@ -9,8 +9,13 @@ CXXFLAGS="-O2 -std=c++14"
 
 #echo "Compiling with C compiler flags: $CXXFLAGS"
 if timeout 20 c++ $CXXFLAGS $fullPath -o $binary
-	then echo -n ""
-	else echo "<font color=\"brown\">Compilation failed!</font>"; exit;
+	then
+		: # nop
+	else
+		echo "<font color=\"brown\">Compilation failed</font>"
+		echo "__SSCK_RES_PACK__"
+		echo "0"
+		exit 1
 fi
 
 ./checkers/$checker $binary $task
